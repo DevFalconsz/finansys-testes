64cfe3b7d112e7a3403ddd35c50add16618820f7
Patrick Oliveira Vagetti <77699722+DevFalconsz@users.noreply.github.com>
2025-10-16 20:43:06 -0300

    Delete tests directory

--- tests/e2e.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('deve carregar a página inicial', async ({ page }) => {
-  await page.goto('http://localhost:5173');
-  await expect(page).toHaveTitle(/Finansys/);
-});
-
